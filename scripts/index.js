44ff861462b386a617e24ee04376c20930a61b35
Azamat Safin <register.uzik@yandex.ru>
2024-12-04 16:16:27 +0500

    Добавили функцию добавления карточек

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,19 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+const cardContainer = document.querySelector(".places__list");
+
+function addCard(card) {
+  const cardTemplate = document.querySelector("#card-template").content;
+  const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
+
+  cardElement.querySelector(".card__title").textContent = card.name;
+  cardElement.querySelector(".card__image").src = card.link;
+
+  cardContainer.append(cardElement);
+}
+
+initialCards.forEach(card => {
+  addCard(card)
+});
