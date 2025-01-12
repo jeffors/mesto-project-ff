d41bdaf02f0b52bd37f768e9bd5a52fd4970d99e
Azamat Safin <register.uzik@yandex.ru>
2025-01-12 22:31:54 +0500

    Фикс добавления карточек через модалку

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -5,6 +5,12 @@ import { openModal, closeModal } from "../components/modal";
 
 const cardContainer = document.querySelector(".places__list");
 const imagePopup = document.querySelector(".popup_type_image");
+const profileAddButton = document.querySelector(".profile__add-button");
+const profileEditButton = document.querySelector(".profile__edit-button");
+const editPopup = document.querySelector(".popup_type_edit");
+const newCardPopup = document.querySelector(".popup_type_new-card");
+const profileTitle = document.querySelector(".profile__title");
+const profileDescription = document.querySelector(".profile__description");
 
 function openImagePopup(evt) {
   imagePopup.querySelector(".popup__image").src = evt.target.src;
@@ -13,16 +19,25 @@ function openImagePopup(evt) {
   openModal(imagePopup);
 }
 
+function handleEditFormSubmit(evt) {
+  evt.preventDefault();
+  profileTitle.textContent = document.forms.edit_profile.elements.name.value;
+  profileDescription.textContent =
+    document.forms.edit_profile.elements.description.value;
+  closeModal(document.querySelector(".popup_is-opened"));
+}
+
+function handleAddFormSubmit(evt) {
+  evt.preventDefault();
+  const placeName = document.forms.new_place.elements.place_name.value;
+  const link = document.forms.new_place.elements.link.value;
+  cardContainer.prepend(addCard({ name: placeName, link: link }, openImagePopup));
+  closeModal(document.querySelector(".popup_is-opened"));
+}
+
 initialCards.forEach((card) => {
   cardContainer.append(addCard(card, openImagePopup));
 });
-
-const profileAddButton = document.querySelector(".profile__add-button");
-const profileEditButton = document.querySelector(".profile__edit-button");
-const editPopup = document.querySelector(".popup_type_edit");
-const newCardPopup = document.querySelector(".popup_type_new-card");
-const profileTitle = document.querySelector(".profile__title");
-const profileDescription = document.querySelector(".profile__description");
 
 profileEditButton.addEventListener("click", () => {
   openModal(editPopup);
@@ -35,22 +50,5 @@ profileAddButton.addEventListener("click", () => {
   openModal(newCardPopup);
 });
 
-function handleEditFormSubmit(evt) {
-  evt.preventDefault();
-  profileTitle.textContent = document.forms.edit_profile.elements.name.value;
-  profileDescription.textContent =
-    document.forms.edit_profile.elements.description.value;
-  closeModal(document.querySelector(".popup_is-opened"));
-}
-
 document.forms.edit_profile.addEventListener("submit", handleEditFormSubmit);
-
-function handleAddFormSubmit(evt) {
-  evt.preventDefault();
-  const placeName = document.forms.new_place.elements.place_name.value;
-  const link = document.forms.new_place.elements.link.value;
-  cardContainer.prepend(addCard({ name: placeName, link: link }, removeCard));
-  closeModal(document.querySelector(".popup_is-opened"));
-}
-
 document.forms.new_place.addEventListener("submit", handleAddFormSubmit);
