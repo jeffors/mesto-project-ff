cee01b624c418494f5a1f3b7e0c87d11538a6a27
Azamat Safin <register.uzik@yandex.ru>
2025-01-27 16:27:41 +0500

    Добавили очистку валидации

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -11,12 +11,14 @@ const editPopup = document.querySelector(".popup_type_edit");
 const newCardPopup = document.querySelector(".popup_type_new-card");
 const profileTitle = document.querySelector(".profile__title");
 const profileDescription = document.querySelector(".profile__description");
-
-function fillProfileForm() {
-  document.forms.edit_profile.elements.name.value = profileTitle.innerText;
-  document.forms.edit_profile.elements.description.value =
-    profileDescription.innerText;
-}
+const validationConfig = {
+  formSelector: ".popup__form",
+  inputSelector: ".popup__input",
+  submitButtonSelector: ".popup__button",
+  inactiveButtonClass: "popup__button_disabled",
+  inputErrorClass: "popup__input_type_error",
+  errorClass: "popup__input_error_active",
+};
 
 function openImagePopup(evt) {
   imagePopup.querySelector(".popup__image").src = evt.target.src;
@@ -46,6 +48,7 @@ function handleAddFormSubmit(evt) {
     )
   );
   document.forms.new_place.reset();
+  clearValidation(newCardPopup, validationConfig);
   closeModal(newCardPopup);
 }
 
@@ -54,20 +57,27 @@ initialCards.forEach((card) => {
 });
 
 profileEditButton.addEventListener("click", () => {
-  fillProfileForm();
+  document.forms.edit_profile.elements.name.value = profileTitle.innerText;
+  document.forms.edit_profile.elements.description.value =
+    profileDescription.innerText;
+  clearValidation(editPopup, validationConfig);
   openModal(editPopup);
-  // TODO: clearValidation()
 });
 
 profileAddButton.addEventListener("click", () => {
-  // TODO: clearValidation()
+  clearValidation(newCardPopup, validationConfig);
   openModal(newCardPopup);
 });
 
 document.forms.edit_profile.addEventListener("submit", handleEditFormSubmit);
 document.forms.new_place.addEventListener("submit", handleAddFormSubmit);
 
-function showInputError(formElement, inputElement, errorMessage, validationConfig) {
+function showInputError(
+  formElement,
+  inputElement,
+  errorMessage,
+  validationConfig
+) {
   const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
   inputElement.classList.add(validationConfig.inputErrorClass);
   errorElement.textContent = errorMessage;
@@ -89,15 +99,24 @@ function checkInputValidity(formElement, inputElement, validationConfig) {
   }
 
   if (!inputElement.validity.valid) {
-    showInputError(formElement, inputElement, inputElement.validationMessage, validationConfig);
+    showInputError(
+      formElement,
+      inputElement,
+      inputElement.validationMessage,
+      validationConfig
+    );
   } else {
     hideInputError(formElement, inputElement, validationConfig);
   }
 }
 
 function setEventListeners(formElement, validationConfig) {
-  const inputList = Array.from(formElement.querySelectorAll(validationConfig.inputSelector));
-  const buttonElement = formElement.querySelector(validationConfig.submitButtonSelector);
+  const inputList = Array.from(
+    formElement.querySelectorAll(validationConfig.inputSelector)
+  );
+  const buttonElement = formElement.querySelector(
+    validationConfig.submitButtonSelector
+  );
 
   toggleButtonState(inputList, buttonElement, validationConfig);
 
@@ -113,7 +132,7 @@ function hasInvalidInput(inputList) {
   return inputList.some((inputElement) => {
     return !inputElement.validity.valid;
   });
-};
+}
 
 function toggleButtonState(inputList, buttonElement, validationConfig) {
   if (hasInvalidInput(inputList)) {
@@ -126,18 +145,26 @@ function toggleButtonState(inputList, buttonElement, validationConfig) {
 }
 
 function enableValidation(validationConfig) {
-  const formList = Array.from(document.querySelectorAll(validationConfig.formSelector));
+  const formList = Array.from(
+    document.querySelectorAll(validationConfig.formSelector)
+  );
   formList.forEach((formElement) => {
     setEventListeners(formElement, validationConfig);
   });
 }
 
-fillProfileForm();
-enableValidation({
-  formSelector: '.popup__form',
-  inputSelector: '.popup__input',
-  submitButtonSelector: '.popup__button',
-  inactiveButtonClass: 'popup__button_disabled',
-  inputErrorClass: 'popup__input_type_error',
-  errorClass: 'popup__input_error_active'
-});
+function clearValidation(profileForm, validationConfig) {
+  const inputList = Array.from(
+    profileForm.querySelectorAll(validationConfig.inputSelector)
+  );
+  const buttonElement = profileForm.querySelector(
+    validationConfig.submitButtonSelector
+  );
+
+  inputList.forEach((inputElement) => {
+    checkInputValidity(profileForm, inputElement, validationConfig);
+  });
+  toggleButtonState(inputList, buttonElement, validationConfig);
+}
+
+enableValidation(validationConfig);
